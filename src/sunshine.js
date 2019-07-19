d6d8e792285d63e782ac947a2becc765c6d44316
Kevin Jing Qiu <kevin@idempotent.ca>
2019-07-19 01:51:55 -0400

    Add all three series

--- src/sunshine.js
+++ src/sunshine.js
@@ -15,10 +15,20 @@ $('a[data-toggle="tab"]').on('shown.bs.tab', async (e) => {
 });
 
 function buildScene1(data, containerSelector) {
+    const getYAxis = (attr, padding, height) => {
+        const maxValue = d3.max(data, d => d[attr]);
+        const minValue = d3.min(data, d => d[attr]);
+
+        const y = d3.scaleLinear()
+            .domain([minValue - padding, maxValue + padding])
+            .range([height, 0]);
+        return y
+    }
+
     const margin = {
         top: 10, right: 30, bottom: 30, left: 60
     };
-    const width = 1024 - margin.left - margin.right;
+    const width = 1000 - margin.left - margin.right;
     const height = 600 - margin.top - margin.bottom;
     const svg = d3.select(containerSelector)
         .append("svg")
@@ -35,51 +45,75 @@ function buildScene1(data, containerSelector) {
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x));
 
-    const maxValue = d3.max(data, d => d.averageSalary);
-    const minValue = d3.min(data, d => d.averageSalary);
-
     // Add Y axis
-    var y = d3.scaleLinear()
-        .domain([minValue - 1000, maxValue + 1000])
-        .range([height, 0]);
-    svg.append("g")
-        .call(d3.axisLeft(y));
+    const y0 = getYAxis('averageSalary', 1000, height);
+    const y1 = getYAxis('totalSalary', 100000000, height);
+    const y2 = getYAxis('totalNumber', 30000, height);
 
-    // Add the line
-    svg.append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", "steelblue")
-        .attr("stroke-width", 2)
-        .attr("d", d3.line()
-            .x(d => x(d.year))
-            .y(d => y(d.averageSalary))
-        );
+    const averageSalaryLine = d3.line().x(d => x(d.year)).y(d => y0(d.averageSalary));
+    const totalSalaryLine = d3.line().x(d => x(d.year)).y(d => y1(d.totalSalary));
+    const totalNumberLine = d3.line().x(d => x(d.year)).y(d => y2(d.totalNumber));
+
+    svg.append("g")
+        .attr('class', 'axisSteelBlue')
+        .call(d3.axisLeft(y0));
+
+    svg.append("g")
+        .attr('class', 'axisOrange')
+        .attr("transform", "translate( " + width + ", 0 )")
+        .call(d3.axisRight(y1));
+
+    svg.append("g")
+        .attr('class', 'axisPurple')
+        .attr("transform", "translate( " + width + ", 0 )")
+        .call(d3.axisLeft(y2));
+
+    const createPath = (color, line) => {
+        svg.append("path")
+            .data([data])
+            .attr("fill", "none")
+            .attr("stroke", color)
+            .attr("stroke-width", 3)
+            .attr("d", line);
+    }
+
+    createPath('steelblue', averageSalaryLine);
+    createPath('orange', totalSalaryLine);
+    createPath('purple', totalNumberLine);
 
     $('div.tooltip').remove();
-    const tooltipDiv = d3.select("body").append("div")	
-        .attr("class", "tooltip")				
-        .style("opacity", 0);
 
-    svg.selectAll('dot')
-       .data(data)
-       .enter()
-       .append('circle')
-       .attr('r', 5)
-       .attr("cx", d => x(d.year))
-       .attr("cy", d => y(d.averageSalary))
-       .on('mouseover', d => {
-           console.log('mouseover');
-           tooltipDiv.transition()
-               .duration(200)
-               .style("opacity", .9);
-           tooltipDiv.html('Year: ' + d.year.getFullYear() + "<br/>" + 'Avg Salary: ' + d.averageSalary.toLocaleString('en-CA', {style: 'currency', currency: 'CAD'}))
-               .style("left", (d3.event.pageX) + "px")
-               .style("top", (d3.event.pageY - 28) + "px");
-       })
-       .on("mouseout", d => {
-            tooltipDiv.transition()
-                .duration(500)
-                .style("opacity", 0);
-        });
+    const createTooltip = (attr, yScale, caption, formatter, color) => {
+        const tooltipDiv = d3.select("body").append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
+
+        // tooltip
+        svg.selectAll('dot')
+            .data(data)
+            .enter()
+            .append('circle')
+            .attr('class', color)
+            .attr('r', 5)
+            .attr("cx", d => x(d.year))
+            .attr("cy", d => yScale(d[attr]))
+            .on('mouseover', d => {
+                tooltipDiv.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                tooltipDiv.html('Year: ' + d.year.getFullYear() + "<br/>" + caption + formatter(d[attr]))
+                    .style("left", (d3.event.pageX + 20) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", d => {
+                tooltipDiv.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });
+    }
+
+    const currencyFormatter = (v) => v.toLocaleString('en-CA', { style: 'currency', currency: 'CAD' });
+    createTooltip('averageSalary', y0, 'Avg Salary: ', currencyFormatter, 'steelBlue');
+    createTooltip('totalSalary', y1, 'Total Salary: ', currencyFormatter, 'orange');
+    createTooltip('totalNumber', y2, 'Total Number on the list: ', v => v, 'purple');
 }
